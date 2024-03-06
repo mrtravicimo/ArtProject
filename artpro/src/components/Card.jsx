864f4803a7121423673d38b7460870b100e15fb8
Travis Racine <mrtravicimo@gmail.com>
2024-03-06 17:50:33 +0900

    create card

---
+++ artpro/src/components/Card.jsx
@@ -0,0 +1,19 @@
+
+const Card = () => {
+  return (
+    <>
+    <div>
+      <div className="card">
+        <h1>hello</h1>
+        <p>
+          this is a solo pro
+        </p>
+        <img src="" alt="" />
+      </div>
+      <button>click here!</button>
+    </div>
+    </>
+  )
+}
+
+export default Card
